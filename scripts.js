3c42262c6f514620560d85a4e09dece30fc39cff
Brenda Neumann <bneumann@stjeromeparish.local>
2019-09-20 15:15:01 -0500

    Refactor while loop with for loop

--- scripts.js
+++ scripts.js
@@ -1,15 +1,10 @@
 // Write a program that sums the numbers from 1 to 10.
 
 //variables
-let counter = 1;
 let sum = 0;
 
-
-// looping
-while (counter <= 10) {
-  sum += counter; // sum = sum + counter;
-
-  counter += 1; // counter = counter + 1;
+for (let i = 1; i <= 10; i += 1) {
+  sum += i; // sum
 }
 
 //write to the console
